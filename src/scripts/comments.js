88c2629b8d3f7295c6bacab54b9a04a4ed2f2d85
Anastasiya <pustoe0@gmail.com>
2020-12-04 13:26:39 +0300

    added html-template for comments

--- src/scripts/comments.js
+++ src/scripts/comments.js
@@ -3,30 +3,37 @@ import {getComments} from "./services/getComments";
 
 export const comments = () =>{
   getComments().then(function(data){
-    showComments(data)
+   showComments(data)
   });
 };
+
 
 const showComments = (comments) =>{
  console.debug($('body'));
   let count = 1
-  let list = document.querySelector('.list-comment')
+  let list = document.querySelector('.wrap')
+    let commentCount = document.querySelector('.comment-count')
   comments.forEach( function(val) {
     if (count <= 10) {
-        let newComment = document.createElement('li')
-        let commentNumber = document.createElement('div')
-        let commentTitle = document.createElement('h3')
-        let commentEmail = document.createElement('span')
-        let commentText = document.createElement('p')
-        commentNumber.textContent = count.toString()
-        commentTitle.textContent = val.name
-        commentEmail.textContent = 'E-mail:  ' + val.email
-        commentText.textContent = val.body
-        newComment.appendChild(commentNumber)
-        newComment.appendChild(commentTitle)
-        newComment.appendChild(commentEmail)
-        newComment.appendChild(commentText)
-        list.appendChild(newComment)
+       list.insertAdjacentHTML('beforeend', `
+        <div class="media">   
+            <a class="pull-left" href="#">
+             <img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+            </a>  
+                <div class="media-body">
+                    <h4 class="media-heading">${ val.name }</h4>
+                    <p>${ val.body }</p>
+                    <ul class="list-unstyled list-inline media-detail pull-left">
+                      <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                      <li><i class="fa fa-thumbs-up"></i>13</li> 
+                    </ul>
+                    <ul class="list-unstyled list-inline media-detail pull-right">
+                       <li class=""><a href="">Like</a></li>
+                       <li class=""><a href="">Reply</a></li>
+                    </ul>
+                </div>
+        </div> `)
+        commentCount.textContent =` ${ count.toString() } Comments `
       }
       count++
     })
