82cdc034ca19ceaf55c70341f2bef04401037f02
Anastasiya <pustoe0@gmail.com>
2020-12-07 20:55:52 +0300

    newComment - new function in a separate file (newComment.js);

--- src/scripts/comments.js
+++ src/scripts/comments.js
@@ -1,41 +1,19 @@
-import $ from 'jquery'
-import {getComments} from "./services/getComments";
+import $ from "jquery";
+import { getComments } from "./services/getComments";
+import { newComment } from "./newComment";
 
-export const comments = () =>{
-  getComments().then(function(data){
-      showComments(sliceArr(data))
+export const comments = () => {
+  getComments().then((data) => {
+    showComments(sliceArr(data, 10));
   });
 };
 
-const sliceArr = (comments) => {
-    return comments.slice(0, 10)
-}
+const sliceArr = (comments, index) => comments.slice(0, index);
 
-
-
-const showComments = (comments) =>{
- console.debug($('body'));
-  let list = document.querySelector('.wrap')
-  comments.forEach( element => {
-      let {email, body} = element
-      list.insertAdjacentHTML('beforeend', `
-        <div class="media">   
-            <a class="pull-left" href="#">
-             <img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
-            </a>  
-                <div class="media-body">
-                    <h4 class="media-heading">${email}</h4>
-                    <p>${body}</p>
-                    <ul class="list-unstyled list-inline media-detail pull-left">
-                      <li><i class="fa fa-calendar"></i>27/02/2014</li>
-                      <li><i class="fa fa-thumbs-up"></i>13</li> 
-                    </ul>
-                    <ul class="list-unstyled list-inline media-detail pull-right">
-                       <li class=""><a href="">Like</a></li>
-                       <li class=""><a href="">Reply</a></li>
-                    </ul>
-                </div>
-        </div> `)
-    })
-}
-
+const showComments = (comments) => {
+  console.debug($("body"));
+  const list = document.querySelector(".wrap");
+  comments.forEach(({ email, body }) => {
+    list.insertAdjacentHTML("beforeend", newComment(email, body));
+  });
+};
---
+++ src/scripts/newComment.js
@@ -0,0 +1,20 @@
+export const newComment = (email, body) => {
+  return `
+        <div class="media">   
+            <a class="pull-left" href="#">
+             <img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+            </a>  
+                <div class="media-body">
+                    <h4 class="media-heading">${email}</h4>
+                    <p>${body}</p>
+                    <ul class="list-unstyled list-inline media-detail pull-left">
+                      <li><i class="fa fa-calendar"></i>27/02/2014</li>
+                      <li><i class="fa fa-thumbs-up"></i>13</li> 
+                    </ul>
+                    <ul class="list-unstyled list-inline media-detail pull-right">
+                       <li class=""><a href="">Like</a></li>
+                       <li class=""><a href="">Reply</a></li>
+                    </ul>
+                </div>
+        </div> `;
+};
