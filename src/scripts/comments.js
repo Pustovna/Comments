03f04a93a9d7b93cfef1c2fb8440825a02633748
Anastasiya <pustoe0@gmail.com>
2020-12-07 20:06:11 +0300

    added sliceArr function, fixed showComments function

--- src/scripts/comments.js
+++ src/scripts/comments.js
@@ -3,26 +3,29 @@ import {getComments} from "./services/getComments";
 
 export const comments = () =>{
   getComments().then(function(data){
-   showComments(data)
+      showComments(sliceArr(data))
   });
 };
+
+const sliceArr = (comments) => {
+    return comments.slice(0, 10)
+}
+
 
 
 const showComments = (comments) =>{
  console.debug($('body'));
-  let count = 1
   let list = document.querySelector('.wrap')
-    let commentCount = document.querySelector('.comment-count')
-  comments.forEach( function(val) {
-    if (count <= 10) {
-       list.insertAdjacentHTML('beforeend', `
+  comments.forEach( element => {
+      let {email, body} = element
+      list.insertAdjacentHTML('beforeend', `
         <div class="media">   
             <a class="pull-left" href="#">
              <img class="media-object" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
             </a>  
                 <div class="media-body">
-                    <h4 class="media-heading">${ val.name }</h4>
-                    <p>${ val.body }</p>
+                    <h4 class="media-heading">${email}</h4>
+                    <p>${body}</p>
                     <ul class="list-unstyled list-inline media-detail pull-left">
                       <li><i class="fa fa-calendar"></i>27/02/2014</li>
                       <li><i class="fa fa-thumbs-up"></i>13</li> 
@@ -33,10 +36,6 @@ const showComments = (comments) =>{
                     </ul>
                 </div>
         </div> `)
-        commentCount.textContent =` ${ count.toString() } Comments `
-      }
-      count++
     })
 }
-
 
